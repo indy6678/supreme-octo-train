1fb4c12d0803ab1acaaa2e77052ee58b1769654c
indy6678 <indy6678@gmail.com>
2022-06-29 23:52:54 -0500

    added time and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
-var now = dayjs();
-console.log(now)
+var today = new Date();
+var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear()
+
+var addDate = function () {
+    $("#currentDay")
+        .text(date);   
+}
+
+var test = $("list-group")
+
+console.log(test.on("click","I clicked"))
+
+var createEvent = function (eventText) {
+    var eventLi = $("list-group")
+    var event = $("<li>").addClass("eventListItem");
+    var eventP = $("<p>").addClass("eventListP").text(eventText)
+    var eventText = $("<textarea>")
+    
+    
+
+    eventLi.appendChild(eventP);
+
+}
+addDate();
+
+
